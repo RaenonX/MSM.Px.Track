30bbfe744c229d8ffeb3805df89c8fbbc7bbc3f6
RaenonX <raenonx0710@gmail.com>
2023-07-16 20:43:38 -0500

    Updated sniping refresh coordinate

--- mod/coordinates.lua
+++ mod/coordinates.lua
@@ -1,7 +1,7 @@
 local base = {}
 
 -- "Buy" at the top bar / navbar
-base.location_refresh = Location(982, 248)
+base.location_refresh = Location(914, 1119)
 base.location_search_bar_focus = Location(475, 386)
 base.location_search_bar_clear = Location(2489, 383)
 base.location_search_bar_search = Location(2645, 385)
